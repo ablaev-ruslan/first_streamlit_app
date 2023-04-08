31b30d10648ee04ef6fbec35c1e17a60eca14b87
Ruslan Ablayev <31989941+ablaev-ruslan@users.noreply.github.com>
2023-04-08 06:57:45 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔 Hard-Boiled Free-Range Egg")
 streamlit.text("🥑🍞 Avocado Toast")
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -25,7 +26,6 @@ streamlit.dataframe(fruits_to_show)
 #New Section
 streamlit.header("Fruityvice Fruit Advice!")
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
